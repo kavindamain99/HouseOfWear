8004cffc8413e20c06d6050acc38244917845c78
MulithaBM <mulithabanukamoremada123@gmail.com>
2022-10-10 11:50:26 +0530

    Sprint 1 - Backend - Address implementation

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
+app.use("/api/addresses", addressRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
